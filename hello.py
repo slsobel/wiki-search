8c00695003a396571ae565a870552f7d9f556c2f
SSobel <sobelsl@gmail.com>
2022-11-27 15:05:14 -0500

    Adds simple formatting

--- hello.py
+++ hello.py
@@ -3,5 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    #return 'Hello, World'
     return render_template('home.html')
+
+@app.route('/*.wiki-search')
+def search():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
