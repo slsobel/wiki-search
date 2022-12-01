ae15a237538fbc9d5b37e2cf009d508c18b7f1d3
SSobel <sobelsl@gmail.com>
2022-12-01 13:05:49 -0500

    adds additional formatting, functionality, notes

--- application.py
+++ application.py
@@ -1,35 +1,42 @@
-from flask import Flask, render_template, request, url_for
+import json
+import wikipedia
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def prompt():
+    return 'Welcome! Please enter the term you would like to search for in the following URL >>> http://127.0.0.1:5000/[YOUR SEARCH TERM].wiki-search.com'
 
-@app.route('/*.wiki-search')
-def search():
-    return render_template('home.html')
-
-@app.route('/<search_term>.wiki-search')
-def results():
-    return render_template('index.html')
-
-'''
-@app.route('/', methods=['GET'])
-def index():
-    if request.method == 'GET':
-        search_term = input('form.value')
+@app.route('/<search>.wiki-search.com')
+def wiki_search(search):
+    
+    # When user follows the prompt message on landing page, it will route to a new page to call Python's Wikipedia library (module imported).
+    
+    url_dict = {'links': []}
+    
+    # New page is directed to display a dictionary of search results, which are dumped into a json (module imported).
+    
+    try:
+        result = wikipedia.page(search, auto_suggest=False).url
+        url_dict['links'].append(result)
         
-        url = request.form['url']
-    return render_template('index.html')
-'''
+    # Script uses try/except block for error handling.
+        
+    except wikipedia.DisambiguationError as e:
+        for option in e.options:
+            try:
+                url = wikipedia.page(option.replace('"', ''), auto_suggest=False).url
+                url_dict['links'].append(url)
+                
+                # For loop takes each url (result) and adds it to the end of the dictionary.
+                
+            except wikipedia.DisambiguationError:
+                pass
+            except wikipedia.PageError:
+                return 'No page matches your search term. Please try searching for a different term.'
+            
+    return json.dumps(url_dict)
 
 if __name__ == '__main__':
-    app.run(debug=True)    
-    
-# gets search term from user input from home search bar
-# uses python input() method 
-        # search_word = input('Search here!') --> or is there a different way to take the form input and insert it into the input()?
-    # validates form input/data using WTForms (works with Flask)
-    # assigns variable name to input as wildcard string literal?
-    # takes user input (variable) and adds to next function/step
-# inserts variable in app.route to make call (GET) using that endpoint (use CLI cURL to test)
+    app.run(debug=True)
